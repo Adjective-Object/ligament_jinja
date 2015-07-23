d5af470fca0a653584f60b4b3eee43aee15c75a5
Adjective-Object <mhuan13@gmail.com>
2015-07-23 16:33:08 -0700

    added filter passthrough, moved output file to build() argument

--- ligament_jinja/__init__.py
+++ ligament_jinja/__init__.py
@@ -7,32 +7,34 @@ from ligament.helpers import mkdir_recursive
 
 class SimpleJinja(BuildTarget):
 
-    def __init__(self, input_file="", output_file="", **kwargs):
+    def __init__(self, template_file="", filters={}, **kwargs):
         BuildTarget.__init__(self, **kwargs)
 
-        self.input_file  = input_file
-        self.output_file = output_file
-
+        self.template_file  = template_file
         self.jinja_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(
-                os.path.dirname(input_file)))
+                os.path.dirname(self.template_file)))
 
-        self.input_basename = os.path.basename(self.input_file)
+        self.jinja_env.filters = filters
 
-        self.file_watch_targets = [os.path.abspath(input_file)]
+        self.input_basename = os.path.basename(self.template_file)
 
-    def build(self, **kwargs):
+        self.file_watch_targets = [os.path.abspath(self.template_file)]
 
-        directory = os.path.dirname(self.output_file)
-        if directory != "" and not os.path.exists(directory):
-            mkdir_recursive(os.path.dirname(self.output_file))
+    def build(self, output_file=None, **kwargs):
+
+        if output_file is not None:
+            directory = os.path.dirname(output_file)
+            if directory != "" and not os.path.exists(directory):
+                mkdir_recursive(os.path.dirname(output_file))
 
         template = self.jinja_env.get_template(self.input_basename)
         html = template.render(
             **kwargs
         )
 
-        with open(self.output_file, 'w') as template_output:
-            template_output.write(html)
+        if output_file is not None:
+            with open(output_file, 'w') as template_output:
+                template_output.write(html)
 
         return html
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='ligament-jinja',
-    version='0.0.0.devl',
+    version='0.0.0.dev',
     description='A jinja build task for ligament',
     url='http://github.com/Adjective-Object/ligament_jinja',
     author='Adjective-Object',
@@ -31,11 +31,11 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
-        'License :: OSI Approved :: Apache 2',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7'],
 
     keywords='ligament grunt build automation jinja',
-    install_requires=['ligament>=0.0.0.devl', 'Jinja2>=2.7.3'],
+    install_requires=['ligament>=0.0.0.dev', 'Jinja2>=2.7.3'],
 
     packages=["ligament_jinja"]
 )
